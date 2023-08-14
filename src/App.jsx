c7bc2392b42a353ad80b04bc438d71d4bb547c20
Rudy Hernandez <rudyhernandez@Rudys-MacBook-Air.local>
2023-08-13 23:34:55 -0700

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 import "./styles.css"
-import { useState } from "react"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
-  function handleSubmit(e) {
-    e.preventDefault()
+    return JSON.parse(localValue)
+  })
 
+  useEffect(() => {
+    localStorage.setItem("ITEM", JSON.stringify(todo))
+  }, [todos])
+
+  function addTodo(title) {
     setTodos(currentTodos => {
-      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false },]
-    })
+         return [
+          ...currentTodos, 
+          {id: crypto.randomUUID(), title, completed: false },
+        ]
+        })
+  }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return {...todo, completed } // how does this change completed to true?
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id) //how does it delete exactly?
+    })
   }
 
   return ( 
   <> 
-  <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item"></label>
-      <input 
-      value = {newItem} 
-      onChange={e => setNewItem(e.target.value)} 
-      type="text"
-      id ="item"
-      />
-    </div>
-    <button className="btn">Add</button>
-  </form>
-  <h1 className="header">Todo List</h1>
-  <ul>
-    {todos.map(todo => {
-      return(
-      <li key={todo.id}>
-        <label>
-          <input type="checkbox" checked={todo.completed} />
-          {todo.title}
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>)
-    })}
-  </ul>
+    <NewTodoForm onSubmit={addTodo}/>
+    <h1 className="header">Todo List</h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </> 
   )
 }
